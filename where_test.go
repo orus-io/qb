7db00561c05011b8e8d58038a8c8104c582e6447
Christophe de Vienne <christophe@cdevienne.info>
2016-09-09 12:24:49 +0200

    Where() now takes a ...Clause

--- where_test.go
+++ where_test.go
@@ -6,6 +6,15 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestWhere(t *testing.T) {
+	assert.Equal(t,
+		"WHERE X", asDefSQL(
+			Where(SQLText("X"))))
+	assert.Equal(t,
+		"WHERE (X AND Y)", asDefSQL(
+			Where(SQLText("X"), SQLText("Y"))))
+}
+
 func TestWhereAnd(t *testing.T) {
 	assert.Equal(t,
 		"WHERE (X AND Y)", asDefSQL(
